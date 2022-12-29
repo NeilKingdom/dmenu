38400694cbd4837bf87fddb41184c2bba96635a2
NeilKingdom <neil@neilkingdom.xyz>
2022-12-29 17:39:21 -0500

    Added lines below prompt patch

--- dmenu.c
+++ dmenu.c
@@ -224,7 +224,7 @@ drawmenu(void)
       int i = 0;
       for (item = curr; item != next; item = item->right, i++)
       {
-         drawitem(item, x + ((i / lines) * ((mw - x) / columns)),
+         drawitem(item, x + ((i / lines) * ((mw - x) / columns)) - promptw,
                   y + (((i % lines) + 1) * bh), (mw - x) / columns);
       }
 	} else if (matches) {
